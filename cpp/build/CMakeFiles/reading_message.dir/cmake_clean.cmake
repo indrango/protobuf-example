d40529d3a6e5d0ecfa60b2a2e6155eabe3c21d62
indrango2 <indra@nodeflux.io>
2018-09-04 09:07:42 +0000

    add cpp code

---
+++ cpp/build/CMakeFiles/reading_message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reading_message.dir/reading_message.cc.o"
+  "reading_message.pdb"
+  "reading_message"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reading_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
